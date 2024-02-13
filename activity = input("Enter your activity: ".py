39493ca98203b1e9c4faa4bdc9f46ce6ccfc3a52
Grace <you@example.com>
2024-02-13 14:20:23 +0300

    initial commit

---
+++ activity = input("Enter your activity: ".py
@@ -0,0 +1,6 @@
+activity = input("Enter your activity: ")  # Asking the user for input
+
+if 'eat' in activity and 'workout' in activity and 'addiction' in activity:
+    print("Proud of you!")
+else:
+    print("Not")
